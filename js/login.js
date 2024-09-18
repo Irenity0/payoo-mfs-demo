df4764b8b26f2e8b20be180efb38d0e7c2c2b715
Irenity <lilithkard77@gmail.com>
2024-09-18 23:02:55 +0600

    added home page

--- js/login.js
+++ js/login.js
@@ -1,7 +1,18 @@
 document.getElementById('button-login').addEventListener('click', function(event){
     event.preventDefault();
-    console.log(`btn btn clicked :3`);
 
     const phoneNum = document.getElementById('phone-number').value;
+    const pinNum = document.getElementById('pin-number').value;
+    
+    
     console.log(phoneNum);
-})
+    console.log(pinNum);
+
+    if(phoneNum === '5' && pinNum === '010101'){
+        console.log('you are logged in');
+        window.location.href = 'home.html';
+    }
+    else{
+        alert('Wrong phone number or pin');
+    }
+}) 
